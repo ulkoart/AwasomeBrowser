9fb3d7ea2605312ac2bc5aceb81461a551ca52b1
Artem Ulko <ulkoart@gmail.com>
2020-02-20 15:16:18 +0300

    Initial Commit

---
+++ AwasomeBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AwasomeBrowser
+//
+//  Created by user on 20/02/2020.
+//  Copyright © 2020 Artem Ulko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
